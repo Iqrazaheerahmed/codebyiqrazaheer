131f5a42b4dd5da96cde26b96be56eb1b31e77ef
Iqrazaheerahmed <iqrazaheerhere@gmail.com>
2024-08-24 20:52:27 +0500

    Add files via upload

---
+++ If else statement/script.js
@@ -0,0 +1,52 @@
+// q no 2
+let marks=prompt("enter your marks(0-100):");
+// // let grade;
+if(marks>=80 && marks <=100){
+    console.log("A1 grade ");
+}
+else if(marks>=70 && marks <=79){
+    console.log(" A grade");
+}
+else if(marks>=60 && marks <=69){
+    console.log("B grade");
+}
+else if(marks>=50 && marks <=59){
+    console.log(" C grade");
+}
+else if(marks>=40 && marks <=49){
+    console.log(" D  grade");
+}
+else if (marks>=0 && marks <=40){
+    console.log("Fail");
+}
+// q no 1
+var acccounting =95;
+var statistics=85;
+var Ecnomics =55;
+var Banking =65;
+var Cost =75;
+var total = 500; 
+var obtain = acccounting +statistics+Ecnomics+Cost+Banking;
+var percentage = (obtain/total)*100;
+console.log("student percentage is" + percentage  + "%");
+if(percentage>=80 && percentage <=100){
+    console.log("A1 grade ");
+}
+ else if(percentage>=70 && percentage <=79){
+    console.log(" A grade");
+}
+else if(percentage>=60 && percentage <=69){
+    console.log("B grade");
+}
+else if(percentage>=50 && percentage <=59){
+    console.log(" C grade");
+}
+else if(percentage>=40 && percentage <=49){
+    console.log(" D  grade");
+}
+else if (percentage>=0 && percentage <=40){
+    console.log("Fail");
+}
+
+
+
